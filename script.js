0db8847d55b810e08023f8d17929b4b04d427e70
hikariii23 <57675891+hikariii23@users.noreply.github.com>
2019-11-15 01:31:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const title="THIS IS WANDA'S ONLINE PORTFOLIO, WELCOME!";
 
-const marqueeText = new Array(50).fill(title).join('🤟🏾');
+const marqueeText = new Array(50).fill(title).join(' 🤟🏾');
 console.log(marqueeText);
 
 const marquee = document.querySelector('.marquee span');
