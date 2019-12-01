70b8da8d50f97f883eedde15f775fc0be6161f7e
hikariii23 <57675891+hikariii23@users.noreply.github.com>
2019-12-02 01:54:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const title="THIS IS WANDA'S ONLINE PORTFOLIO, WELCOME!";
+// const title="THIS IS WANDA'S ONLINE PORTFOLIO, WELCOME!";
+//
+// const marqueeText = new Array(50).fill(title).join(' 🤟🏾');
+// console.log(marqueeText);
+//
+// const marquee = document.querySelector('.marquee span');
+//
+// marquee.innerHTML= marqueeText;
+$(function () {
+    $('.marquee').marquee({
+         duration:5000,
+         duplicated: true,
+         gap: 50,
+         direction: 'left',
+         pauseOnHover: false
 
-const marqueeText = new Array(50).fill(title).join(' 🤟🏾');
-console.log(marqueeText);
-
-const marquee = document.querySelector('.marquee span');
-
-marquee.innerHTML= marqueeText;  
+    });
+});
